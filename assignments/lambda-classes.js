bf7ed9bfe94514a414ee8f59f17a09844b695b74
tjr226 <tjr226@gmail.com>
2019-04-04 11:03:15 -0400

    added Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,28 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+
+// * Instructor has the following unique props:
+//   * `specialty` what the Instructor is good at i.e. 'redux'
+//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+//   * `catchPhrase` i.e. `Don't forget the homies`
+// * Instructor has the following methods:
+//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+
+class Instructor extends Person {
+    constructor(instructorAttrs) {
+        super(instructorAttrs);
+        this.specialty = instructorAttrs.specialty,
+        this.favLanguage = instructorAttrs.favLanguage,
+        this.catchPhrase = instructorAttrs.catchPhrase,
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}.`
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
+}
+
