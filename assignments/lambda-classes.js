23f134f606276413c21d52e863c76064d8cb3238
tjr226 <tjr226@gmail.com>
2019-04-04 11:09:53 -0400

    added student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,9 +3,9 @@
 class Person {
     constructor(personAttrs) {
         this.name = personAttrs.name,
-        this.age = personAttrs.age,
-        this.location = personAttrs.location,
-        this.gender = personAttrs.gender;
+            this.age = personAttrs.age,
+            this.location = personAttrs.location,
+            this.gender = personAttrs.gender;
     }
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
@@ -25,8 +25,8 @@ class Instructor extends Person {
     constructor(instructorAttrs) {
         super(instructorAttrs);
         this.specialty = instructorAttrs.specialty,
-        this.favLanguage = instructorAttrs.favLanguage,
-        this.catchPhrase = instructorAttrs.catchPhrase,
+            this.favLanguage = instructorAttrs.favLanguage,
+            this.catchPhrase = instructorAttrs.catchPhrase,
     }
     demo(subject) {
         return `Today we are learning about ${subject}.`
@@ -35,4 +35,26 @@ class Instructor extends Person {
         return `${student.name} receives a perfect score on ${subject}.`
     }
 }
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground,
+            this.className = studentAttrs.className,
+            this.favSubjects = studentAttrs.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.forEach(function (element) {
+            console.log(element)
+        });
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}.`
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`
+    }
+
+}
+
 
